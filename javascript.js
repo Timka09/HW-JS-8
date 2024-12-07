d8c26f73f1bacc1e233f896d77ee593ff3a8a027
Timka09 <tymofij.i.naumenko@gmail.com>
2024-12-07 17:14:46 +0200

    dz update

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
 // Task 1
 // join()
-const barysNegar = ["Mango", "Poly", "Kiwi", "Ajax"]
-let OlaZhopa = barysNegar.join(", ")
-console.log(OlaZhopa)
+const fruitArray = ["Mango", "Poly", "Kiwi", "Ajax"]
+let joinToArray = fruitArray.join(", ");
+console.log(joinToArray)
 // for
-// let rydok = barysNegar.join(", ")
-// for (let i = 0; barysNegar.length; i++) {
-//     rydok += barysNegar[i]
-// }
-// console.log(rydok)
+let res = ""
+for (let i = 0; i < fruitArray.length; i++) {
+    res += fruitArray[i] + ", "
+} 
+console.log(res)
 // Task 2
-const trellaalalala = ["Karta 1", "Karta 2", "Karta 3", "Karta 4", "Karta 5"];
-const treleleleRemove = trellaalalala.shift()
-console.log(trellaalalala)
+const arrayCards = ["Karta 1", "Karta 2", "Karta 3", "Karta 4", "Karta 5"];
+const removeCards = arrayCards.shift()
+console.log(arrayCards)
 // Task 3
-const joinKarta = trellaalalala.push("Karta 6")
-console.log(trellaalalala)
+const joinCard = arrayCards.push("Karta 6")
+console.log(arrayCards)
 // Task 4
-const kartaToUpadate = trellaalalala.splice(1, 1, "Karta nie3")
-console.log(trellaalalala)
+const cardToUpadate = arrayCards.splice(1, 1, "Karta nie3")
+console.log(arrayCards)
