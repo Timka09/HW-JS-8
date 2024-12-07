8e422de930195b4d06e2225e8ba7f43a39fdcc30
Timka09 <tymofij.i.naumenko@gmail.com>
2024-12-07 12:41:10 +0200

    dz

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// Task 1
+// join()
+const barysNegar = ["Mango", "Poly", "Kiwi", "Ajax"]
+let OlaZhopa = barysNegar.join(", ")
+console.log(OlaZhopa)
+// for
+// let rydok = barysNegar.join(", ")
+// for (let i = 0; barysNegar.length; i++) {
+//     rydok += barysNegar[i]
+// }
+// console.log(rydok)
+// Task 2
+const trellaalalala = ["Karta 1", "Karta 2", "Karta 3", "Karta 4", "Karta 5"];
+const treleleleRemove = trellaalalala.shift()
+console.log(trellaalalala)
+// Task 3
+const joinKarta = trellaalalala.push("Karta 6")
+console.log(trellaalalala)
+// Task 4
+const kartaToUpadate = trellaalalala.splice(1, 1, "Karta nie3")
+console.log(trellaalalala)
